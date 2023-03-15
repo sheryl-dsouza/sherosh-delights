b3a50a302a7e7fefade95c54c20375a83e19a978
Sheryl Dsouza <sheryldsouzs@MacBook-Air.local>
2023-03-14 23:03:47 -0500

    adding catch phrase

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Text from '../home.png'
+import './Home.css'
 
 function Home() {
     return (
         <div>
         <Row>
-          <Col style={{background:'#FF89AD', minHeight:'1000px'}}></Col>
+          <Col style={{background:'#FF89AD', minHeight:'1000px'}}><img class="full-width" src={Text}></img></Col>
           <Col style={{background:'#FFB4C6'}}><br></br></Col>
         </Row>
       </div>
