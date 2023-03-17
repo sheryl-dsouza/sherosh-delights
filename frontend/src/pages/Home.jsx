42c2b2779858b7005d7464ff7d5481531caa52e4
Sheryl Dsouza <sheryldsouzs@MacBook-Air.local>
2023-03-17 00:55:53 -0500

    fixing home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,11 +14,11 @@ function Home() {
     return (<div className="background">
     <div >
   <Row>
-    <Col style={{background:'#FF89AD', opacity:'.95', borderRadius:'20px', boxShadow: '0px 0px 20px 0px rgba(128, 29, 82, 0.5)', margin:'5%'}}>
-    <img src={Cake} style={{borderRadius:'20px', marginTop:'5%',marginLeft:'auto', marginRight:'auto', display:'block'}}></img>
-      <p className="para" style={{marginLeft:'auto', marginRight:'auto', display:'block', textAlign:'center', fontSize:'30px'}}>Welcome to Sherosh Delights!</p>
+    <Col style={{background:'#FF89AD', opacity:'.95', borderRadius:'20px', boxShadow: '0px 0px 20px 0px rgba(128, 29, 82, 0.5)', margin:'5% 12% 5% 12%'}}>
+    <img src={Cake} style={{borderRadius:'20px', marginTop:'5%',marginLeft:'auto', marginRight:'auto', display:'block', width:'80%'}}></img>
+      <p className="para" style={{marginLeft:'auto', marginRight:'auto', display:'block', textAlign:'center', fontSize:'100%'}}>Welcome to Sherosh Delights!</p>
       <Link activeClass="active" smooth spy to="menu">
-        <img src={Menu} class="other" style={{marginLeft:'auto', marginRight:'auto', display:'block', blockSize:'10%'}}/> 
+        <img src={Menu} class="other" style={{marginLeft:'auto', marginRight:'auto', display:'block', blockSize:'5%'}}/> 
       </Link>        
   </Col>
   </Row>
