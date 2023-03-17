2533b29131363807342f018ad5a7a7f68ae09887
Sheryl Dsouza <sheryldsouzs@MacBook-Air.local>
2023-03-16 23:47:25 -0500

    debug website deploying

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
           <Col style={{background:'#FF89AD', minHeight:'700px', opacity:'.95', borderRadius:'20px 0 0 20px', boxShadow: '0px 0px 20px 0px rgba(128, 29, 82, 0.5)' }}>
             <img class="full-width" src={Text}></img>
             <p className="para">Welcome to Sherosh Delights!</p>
-            <p className="para">At Sherosh Delights, I specialize in creating delicious and beautifully decorated cakes for all occasions. From birthdays to weddings, baby showers to anniversaries, I've got you covered. My cakes are baked fresh using only the finest ingredients, and I take pride in creating custom designs to match your vision.</p>
+            <p className="para">At Sherosh Delights, I specialize in creating delicious and beautifully decorated cakes for all occasions. From birthdays to baby showers to anniversaries, I've got you covered. My cakes are baked fresh using only the finest ingredients, and I take pride in creating custom designs to match your vision.</p>
             <p className="para">Check out our menu page for a selection of our flavors, fillings, and icings to choose from. I offer speciality cakes, as well as other desserts. Lastly, if you have something in mind that is not listed, let me know! You dream it, I make it. </p>
             <Link activeClass="active" smooth spy to="menu" className="menu">
               <img src={Menu} class="other"/> 
